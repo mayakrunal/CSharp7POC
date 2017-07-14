9d752679e8729811d8e4a05dddeef909f225be1b
Krunal Parekh <maya.krunal@gmail.com>
2017-07-14 14:13:39 +0800

    initial

---
+++ CSharp7POC/CSharp7POC/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp7POC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (int.TryParse(Console.ReadLine(), out var answer))
+                Console.WriteLine(answer);
+            else
+                Console.WriteLine("Could not parse input");
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp7POC/Tuples/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = ("a", "b");
+
+            (string Alpha, string Beta) namedLetters = ("a", "b");
+
+            var alphaStart = (Alpha: "a", Beta: "b");
+
+            var range = Range(new int[] { 1,2,3});
+
+            (int max, int min) = Range(new int[] { 1, 2, 3 });
+
+
+        }
+
+        private static (int Max, int Min) Range(IEnumerable<int> Numbers)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            foreach (var n in Numbers)
+            {
+                min = (n < min) ? n : min;
+                max = (n > max) ? n : max;
+            }
+            return (max, min);
+        }
+    }
+}
